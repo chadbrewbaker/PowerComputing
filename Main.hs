398880b75c9c8db2fd5a6f372cbdb1f6adcefc66
Chad Brewbaker <crb002@gmail.com>
2015-10-17 18:03:46 -0500

    some more n-nacci numbers

--- Main.hs
+++ Main.hs
@@ -7,4 +7,8 @@ main = do
 	print "Ones 1/(1-z)"
 	print (take 10 (1/(1-z)))
         print "Fibonacci 1/(1-z-z^2)"
-	print (take 10 (1/(1-z-(z*z)))) 
+	print (take 10 (1/(1-z-(z*z))))
+	print "Tribonacci numbers"
+	print (take 10 (1/(1-z-(z*z)-(z*z*z))))
+	print "Tetranacci numbers?"
+	print (take 10 (1/(1-z-(z*z)-(z*z*z)-(z*z*z*z)))) 
